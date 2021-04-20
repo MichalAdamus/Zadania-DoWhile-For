a132b5b95e2b97dee38849add7fb6e229ddfebf3
Michał Adamus <mb.adamus@gmail.com>
2021-04-20 19:57:12 +0200

    #5 Dzielenie przez 2 liczb od 62349 do 62392

---
+++ src/DisplayNumbersFor2.java
@@ -0,0 +1,8 @@
+public class DisplayNumbersFor2 {
+    public static void main(String[] args) {
+        for (int i = 62349; i <= 62392 ; i++) {
+            if (i%2==0)
+            System.out.println(i);
+        }
+    }
+}
