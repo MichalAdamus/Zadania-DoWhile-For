a606b8bc5d495ba8db00de2af151f0130d2b6d4c
Michał Adamus <mb.adamus@gmail.com>
2021-04-21 20:41:52 +0200

    #1 ręczne wyswietlenie reszty z dzielenia

--- src/DisplayNumbers.java
+++ src/DisplayNumbers.java
@@ -1,10 +1,33 @@
 public class DisplayNumbers {
     public static void main(String[] args) {
-        int number = 25;
+//        int [] number = {21,10,5,2,1,0};
+//        int i =1;
+//        do {
+//            System.out.println("Reszta z Dzielenia =" + number[i]);
+//            i++;
+//        }
+//        while (i < number.length && number[i] >=0);
+//    }
+//}
+//        int a = 21 % 2;
+//        int b = 10 % 2;
+//        int c = 5 % 2;
+//        int d = 2 % 2;
+//        int e = 1 % 2;
+//        System.out.print(a);
+//        System.out.print(b);
+//        System.out.print(c);
+//        System.out.print(d);
+//        System.out.print(e);
+//    }
+//}
+
+        int number = 12;
         do {
-            number = number / 2;
-            System.out.println("Wynik Dzielenia =" + number);
+            number = number %= 2;
+            System.out.println("Reszta z Dzielenia =" + number);
         }
-        while (number > 0);
+        while (number < 0);
     }
 }
+
