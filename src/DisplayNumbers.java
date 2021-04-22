3752f3afaee88307b06cdbe2f7bd431c84d8d147
Michał Adamus <mb.adamus@gmail.com>
2021-04-22 08:54:47 +0200

    #21 Rozwiazanie zadania dzielnia liczb z resztą

--- src/DisplayNumbers.java
+++ src/DisplayNumbers.java
@@ -1,33 +1,15 @@
 public class DisplayNumbers {
     public static void main(String[] args) {
-//        int [] number = {21,10,5,2,1,0};
-//        int i =1;
-//        do {
-//            System.out.println("Reszta z Dzielenia =" + number[i]);
-//            i++;
-//        }
-//        while (i < number.length && number[i] >=0);
-//    }
-//}
-//        int a = 21 % 2;
-//        int b = 10 % 2;
-//        int c = 5 % 2;
-//        int d = 2 % 2;
-//        int e = 1 % 2;
-//        System.out.print(a);
-//        System.out.print(b);
-//        System.out.print(c);
-//        System.out.print(d);
-//        System.out.print(e);
-//    }
-//}
 
-        int number = 12;
+        int number = 25;
         do {
-            number = number %= 2;
-            System.out.println("Reszta z Dzielenia =" + number);
+            int result = number / 2;
+            int modulo = number % 2;
+//            System.out.println(number + " / 2 = " + result + " reszta " + modulo );
+            System.out.print(modulo);
+            number = result;
         }
-        while (number < 0);
+        while (number > 0);
     }
 }
 
